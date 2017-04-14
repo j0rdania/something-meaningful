2831d25dc6b34f1423c7a8760c51b3dd6ed6a647
Jordan Levine <email": null>
2017-04-14 15:37:05 -0700

    Change styling and add h2

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -1,8 +1,10 @@
 get '/' do
   # @quote = Quote.quotation_text.sample
-  num_quotes = Quote.count
-  all_quotes=Quote.all
-  p all_quotes
-  @quote = all_quotes.find(rand(0...num_quotes))
+  # num_quotes = Quote.count
+  # all_quotes=
+  # Quote.all.sample
+  # p all_quotes
+  # @quote = all_quotes.find(rand(1...num_quotes))
+  @quote = Quote.all.sample
   erb :'/index'
 end
