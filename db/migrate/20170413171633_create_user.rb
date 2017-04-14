ee0910f8b9963bc34c6bc34aa1c3fa8fbf466c50
Jordan Levine <email": null>
2017-04-13 17:17:34 -0700

    add user model and migration

---
+++ db/migrate/20170413171633_create_user.rb
@@ -0,0 +1,16 @@
+class CreateUser < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :password_hash, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
+
+
+
+
+
