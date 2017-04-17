09c320f28003de55ccc1baea83d476f723a20ba0
Jordan Levine <email": null>
2017-04-17 15:23:58 -0700

    remove ads function written

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -18,39 +18,34 @@ def cowify(string_to_cowify,html_or_text)
   return body
 end
 
-def cowify_strip_ads(string_to_cowify,html_or_text)
-  string_to_cowify = string_to_cowify.gsub(' ','%20')
-  string_to_cowify = "http://cowsay.morecode.org/say?message=#{string_to_cowify}&format=#{html_or_text}"
-  # uri = URI.parse(string_to_cowify)
-  # req = Net::HTTP::Get.new(uri.to_s)
-  # response = Net::HTTP.get_response(uri)
-  # body = Net::HTTP.get(uri)
-  body = Nokogiri::HTML(RestClient.get(string_to_cowify))
-
-  return body
+def get_string_between_markers whole_string, marker1, marker2
+  whole_string[/#{Regexp.escape(marker1)}(.*?)#{Regexp.escape(marker2)}/m, 1]
 end
 
-def get_middle_string(whole_string,first_string, second_string)
-  # return the string that lies between the first and second string
-  # chop off end
-  fragment =  Nokogiri::HTML.fragment(whole_string)
-
+def strip_ads(string_to_de_ad)
+  # body = Nokogiri::HTML(RestClient.get(string_to_cowify))
+  body = get_string_between_markers(string_to_de_ad,'MOO!</a></h1>','</html>')
+  puts 'BODY TO FOLLOW'
+  p body
+  return body
 end
 
 get '/' do
   # get random quote from database
   quote_to_use = Quote.all.sample.quotation_text
+  puts 'QUOTE TO USE TO Follow'
+  p quote_to_use
   # quote_to_use = quote_to_use.quotation_text.gsub(' ','%20')
-  #convert quote of day to cow say
+  # # convert quote of day to cow say
   # quote_to_cowify = "http://cowsay.morecode.org/say?message=#{quote_to_use}&format=html"
   # uri = URI.parse(quote_to_cowify)
   # req = Net::HTTP::Get.new(uri.to_s)
   # response = Net::HTTP.get_response(uri)
   # body = Net::HTTP.get(uri)
-  # @cow_quote = cowify_strip_ads(quote_to_use,'html')
-  @cow_quote="HI"
+  @cow_quote = cowify(quote_to_use,'html')
+  # @cow_quote=strip_ads(@cow_quote)
 
-  # get current weather at Bull Frog station
+  #get current weather at Bull Frog station
   weather_request = 'http://wsdot.com/Traffic/api/WeatherInformation/WeatherInformationREST.svc/GetCurrentWeatherInformationByStationIDAsJson?AccessCode=50bf2668-84ee-4983-bc54-f6dae3a5a31d&StationID=3002'
   uri = URI.parse(weather_request)
   request = Net::HTTP::Get.new(uri.to_s)
@@ -62,14 +57,15 @@ get '/' do
   @cow_weather = "Temperature in #{@station_name}: #{@temp} degrees Farenheit.  Woohoo! What a lovely day!"
   # @cow_weather = @cow_weather.gsub(' ','%20')
 
-  #convert weather report to cow say
-  @cow_weather = cowify_strip_ads(@cow_weather,'html')
+  # convert weather report to cow say
+  @cow_weather = cowify(@cow_weather,'html')
   # weather_to_cowify = "http://cowsay.morecode.org/say?message=#{@cow_weather}&format=html"
   # uri = URI.parse(weather_to_cowify)
   # req = Net::HTTP::Get.new(uri.to_s)
   # response = Net::HTTP.get_response(uri)
   # body = Net::HTTP.get(uri)
   # @cow_weather = body
+
   erb :'/index'
 end
 
